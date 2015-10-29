45cb358e8ce9cb653360ea7cf65b70ad7ca85b43
Paul Boyle <pboyle17@gmail.com>
2015-10-29 16:07:09 -0500

    updating ..

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// hey hey hey
+
+window.onload= function(){
+  //alert('hey hey hey');
+  statusReport('Hey hey hey!!!');
+
+}
+
+function statusReport(message){
+  console.log(message);
+  return message;
+}
