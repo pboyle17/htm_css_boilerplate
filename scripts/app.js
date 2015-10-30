acc485a57077302e4e0bcfee0016c7cfc72b0eea
Paul Boyle <pboyle17@gmail.com>
2015-10-29 19:09:13 -0500

    worked with html selector to make mad libs type output

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,17 @@ window.onload= function(){
   //alert('hey hey hey');
   statusReport('Hey hey hey!!!');
 
+  var username = prompt('What is your name?');
+  var mission = prompt('this is the next thing?');
+  var x = prompt('dfkjdfa');
+  var y=prompt('lka;sjfklfjdf');
+  var z=prompt('kdfjfsalf');
+
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to' + mission+'lkjfldsajflas'+x+'lkfjdsafjasf'+'fjkasfjask;f'+y+'dfjsdalfajdlsfsf'+z+'dfjsdl;asf';
+
 }
 
 function statusReport(message){
